0a06bf0696c43127634f7ad33daea5d35779e43f
hekai <1208730147@qq.com>
2018-06-13 08:53:44 +0800

    require

---
+++ src/js/app/index.js
@@ -0,0 +1,4 @@
+define(['jquery'], function($) {
+    $('.wrap').css('background', 'pink');
+    $('.wrap').html('返回成功');
+})
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+require.config({
+    baseUrl: './js/',
+    paths: {
+        //库文件
+        'jquery': 'lib/jquery-2.1.1.min',
+        //自定义
+        'index': 'app/index'
+    }
+})
